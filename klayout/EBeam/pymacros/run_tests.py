5df9bfe892488c031e47c3242d38226afceca8e6
Fariha Shaikh <fariha@voltsafe.com>
2023-11-23 14:25:55 -0800

    Trying with a .coveragerc file

--- klayout/EBeam/pymacros/run_tests.py
+++ klayout/EBeam/pymacros/run_tests.py
@@ -1,8 +1,12 @@
 # run_tests.py
 import subprocess
+import os
 
-# Run your tests with coverage
-subprocess.run(['klayout', '-zz', '-r', 'coverage', 'run', '--source=/home/pymacros', 'pymacros/EBeam_Lib_PCellTests.py'], check=True)
+# Set the COVERAGE_PROCESS_START environment variable
+os.environ['COVERAGE_PROCESS_START'] = '/home/pymacros/.coveragerc'
+
+# Run your tests with coverage using klayout
+subprocess.run(['klayout', '-zz', '-r', 'pymacros/EBeam_Lib_PCellTests.py'], check=True)
 
 # Generate the coverage report
 subprocess.run(['coverage', 'xml', '-o', '/home/pymacros/coverage.xml'], check=True)
