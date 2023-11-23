a0414a2e4bae27bf514a97065333bbabb8bdd8d9
Fariha Shaikh <fariha@voltsafe.com>
2023-11-23 11:54:14 -0800

    Testing code coverage in actions file using new python script containing subprocess commands

---
+++ klayout/EBeam/pymacros/run_tests.py
@@ -0,0 +1,8 @@
+# run_tests.py
+import subprocess
+
+# Run your tests with coverage
+subprocess.run(['klayout', '-zz', '-r', 'coverage run --source=/home/pymacros pymacros/EBeam_Lib_PCellTests.py'], check=True)
+
+# Generate the coverage report
+subprocess.run(['coverage', 'xml', '-o', '/home/pymacros/coverage.xml'], check=True)
