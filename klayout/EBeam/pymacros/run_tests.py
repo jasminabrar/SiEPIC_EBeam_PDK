eb36c80b50e72ebae10a914bcb917d86fa10043e
Fariha Shaikh <fariha@voltsafe.com>
2023-11-23 15:23:22 -0800

    Adding missing coverage command back in again

--- klayout/EBeam/pymacros/run_tests.py
+++ klayout/EBeam/pymacros/run_tests.py
@@ -6,7 +6,19 @@ import os
 os.environ['COVERAGE_PROCESS_START'] = '/home/pymacros/.coveragerc'
 
 # Run your tests with coverage using klayout
-subprocess.run(['klayout', '-zz', '-r', 'pymacros/EBeam_Lib_PCellTests.py'], check=True)
+try:
+    subprocess.run(['coverage', 'run', 'klayout', '-zz', '-r', 'pymacros/EBeam_Lib_PCellTests.py'], check=True)
+
+except subprocess.CalledProcessError as e:
+    print(f"Error during test execution: {e}")
+    # Print the contents of the KLayout log
+    klayout_log_path = '/tmp/klayout.log'
+    with open(klayout_log_path, 'r') as log_file:
+        print(log_file.read())
+    raise  # Re-raise the exception
+
+else:
+    print("Tests completed successfully.")
 
 # Generate the coverage report
 subprocess.run(['coverage', 'xml', '-o', '/home/pymacros/coverage.xml'], check=True)
