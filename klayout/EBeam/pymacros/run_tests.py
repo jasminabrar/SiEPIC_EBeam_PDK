ad3828774e698ecf32153b81f3b2301f0c0a3d4e
Fariha Shaikh <fariha@voltsafe.com>
2023-11-23 13:45:45 -0800

    Updated run_tests.py to have separated commands

--- klayout/EBeam/pymacros/run_tests.py
+++ klayout/EBeam/pymacros/run_tests.py
@@ -2,7 +2,7 @@
 import subprocess
 
 # Run your tests with coverage
-subprocess.run(['klayout', '-zz', '-r', 'coverage run --source=/home/pymacros pymacros/EBeam_Lib_PCellTests.py'], check=True)
+subprocess.run(['klayout', '-zz', '-r', 'coverage', 'run', '--source=/home/pymacros', 'pymacros/EBeam_Lib_PCellTests.py'], check=True)
 
 # Generate the coverage report
 subprocess.run(['coverage', 'xml', '-o', '/home/pymacros/coverage.xml'], check=True)
